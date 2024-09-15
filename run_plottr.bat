4f097024af802f009f00e7f5a9f100925bf74770
KeLab <quantumkelab@gmail.com>
2024-09-15 17:05:15 +0800

    v0.0.12

--- run_plottr.bat
+++ run_plottr.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Activate the Conda environment
-call conda activate py3.11
+call conda activate qcodes
 
 REM Run plottr-inspectr with the provided database path
 plottr-inspectr --dbpath=%1
