ce36211b6be28fe117f0279b8e162393ab67e2cd
KeLab <quantumkelab@gmail.com>
2025-01-03 19:45:08 +0800

    v0.0.31

--- fridge_specific/init_BF1_v2.py
+++ fridge_specific/init_BF1_v2.py
@@ -7,6 +7,7 @@ from qcodes.instrument_drivers.Keithley.Keithley_2400 import Keithley2400
 from qcodes.instrument_drivers.tektronix.Keithley_6500 import Keithley_6500
 from qcodes.instrument_drivers.rohde_schwarz.SGS100A import RohdeSchwarzSGS100A
 from qcodes.instrument_drivers.american_magnetics.AMI430 import AMI430, AMI430_3D
+from qcodes_contrib_drivers.drivers.QuTech.IVVI import IVVI
 
 # 初始化站點
 station = qc.Station()
